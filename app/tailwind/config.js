4de3c8574f2c26f66fae139309d70ee485b75f34
Cosmin Stamate <cos.stamate@gmail.com>
2019-06-12 22:38:46 +0300

    Add better disabled fields and button support

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -418,6 +418,14 @@ module.exports = {
       '40': '40',
       '50': '50',
     },
+    spinner: {
+      default: {
+        color: '#dae1e7', // color you want to make the spinner
+        size: '1em', // size of the spinner (used for both width and height)
+        border: '2px', // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+        speed: '500ms', // the speed at which the spinner should rotate
+      },
+    },
   },
   variants: {
     alignContent: ['responsive'],

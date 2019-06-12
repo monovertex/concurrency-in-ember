b1f7e49df074ad11c8a4787f6e1e06112d8b9bbf
Cosmin Stamate <cos.stamate@gmail.com>
2019-06-13 00:34:10 +0300

    Implement part 1 using observables

--- app/components/part-1/observables.js
+++ app/components/part-1/observables.js
@@ -1,7 +1,35 @@
 import Component from '@ember/component';
+import { bind } from '@ember/runloop';
+import { inject } from '@ember/service';
+import { fromEvent, from } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
 
 export default Component.extend({
-  layoutName: 'components/part-1/base',
+  flashMessages: inject(),
 
-  pageTitle: '#1. Basic Form Saving: observables',
+  didInsertElement() {
+    this._super(...arguments);
+    this.initializeFormSubscription();
+  },
+
+  willDestroyElement() {
+    this.formSubscription.unsubscribe();
+  },
+
+  initializeFormSubscription() {
+    const submitStream = fromEvent(this.element.querySelector('#form-book'), 'submit', true);
+    const saveFinishedStream = submitStream.pipe(flatMap((...args) => from(this.onSave(...args))));
+    const subscription = saveFinishedStream.subscribe(bind(this, 'onSaveFinished'));
+    this.set('formSubscription', subscription);
+  },
+
+  onSave() {
+    this.set('isSaving', true);
+    return this.model.save();
+  },
+
+  onSaveFinished() {
+    this.flashMessages.success('Book saved successfully');
+    this.set('isSaving', false);
+  },
 });

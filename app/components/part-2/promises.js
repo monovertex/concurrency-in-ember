f76557fe4b53775079fff1c8b6f990cfcaa156f0
Cosmin Stamate <cos.stamate@gmail.com>
2019-06-13 12:13:10 +0300

    Implement part 2 with promises

--- app/components/part-2/promises.js
+++ app/components/part-2/promises.js
@@ -1,4 +1,40 @@
 import Component from '@ember/component';
+import { readOnly } from '@ember/object/computed';
+import { inject } from '@ember/service';
 
 export default Component.extend({
+  flashMessages: inject(),
+
+  isSaving: false,
+  isSavingAsDraft: false,
+  isDraft: readOnly('model.isDraft'),
+
+  async saveModelAsDraft() {
+    this.set('isSavingAsDraft', true);
+    this.model.set('isDraft', true);
+
+    try {
+      await this.model.save();
+    } finally {
+      this.set('isSavingAsDraft', false);
+    }
+  },
+
+  actions: {
+    async onSave() {
+      this.set('isSaving', true);
+      try {
+        await this.model.save();
+        this.model.set('isDraft', false);
+        this.flashMessages.success('Book saved successfully');
+      } finally {
+        this.set('isSaving', false);
+      }
+    },
+
+    onChange(key, value) {
+      this.model.set(key, value);
+      this.saveModelAsDraft();
+    }
+  }
 });

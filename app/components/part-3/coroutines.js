456355bc5c6f86981afed20627811c6f3e1fd1bc
Cosmin Stamate <cos.stamate@gmail.com>
2019-06-13 14:51:14 +0300

    Implement part 3 using coroutines

--- app/components/part-3/coroutines.js
+++ app/components/part-3/coroutines.js
@@ -1,4 +1,39 @@
 import Component from '@ember/component';
+import { readOnly } from '@ember/object/computed';
+import { inject } from '@ember/service';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
+  flashMessages: inject(),
+
+  saveTask: task(function * () {
+    try {
+      yield this.draftSaveTask.last;
+    } catch (e) {
+      // Do nothing.
+    }
+
+    this.model.set('isDraft', false);
+    yield this.model.save();
+    this.flashMessages.success('Book saved successfully');
+  }).drop(),
+
+  draftSaveTask: task(function * () {
+    yield timeout(500);
+    this.model.set('isDraft', true);
+    return yield this.model.save();
+  }).restartable(),
+
+  isSaving: readOnly('saveTask.isRunning'),
+  isSavingAsDraft: readOnly('draftSaveTask.isRunning'),
+  isDraft: readOnly('model.isDraft'),
+
+  actions: {
+    onSave() { return this.saveTask.perform(); },
+
+    onChange(key, value) {
+      this.model.set(key, value);
+      return this.draftSaveTask.perform();
+    },
+  }
 });
